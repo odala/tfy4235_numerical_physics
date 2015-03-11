83022f6c702596f54091ac66206433f49e401543
odala <odalauten@gmail.com>
2015-03-11 09:00:40 +0100

    Task: drift velocity and representation of data - Trajectories are now represented by vector instead of matrix. - Drift velocities are collected for tau \in [0, 4.5] with resolution 0.01.

--- assignment_01/a1.f90
+++ assignment_01/a1.f90
@@ -19,7 +19,7 @@ program biased_brownian_motion
     
     ! ######## Variables #########    
     integer, parameter                  :: N = 100
-    integer, parameter                  :: nSteps = 9999999
+    integer, parameter                  :: nSteps = 1100000
     real(wp)                            :: r                    !// m
     real(wp)                            :: x0
     real(wp)                            :: kT                   !// eV
@@ -29,18 +29,22 @@ program biased_brownian_motion
     real(wp)                            :: tau                  !// the period of the flash
     real(wp)                            :: zeta, gama, omega    !// dynamic viscosity, friction constant, kT/dU, dU/gama/L**2
     real(wp)                            :: dt
-    real(wp), dimension(0:N-1,0:nSteps-1):: x
+    real(wp), dimension(0:nSteps-1)     :: x
+    real(wp), dimension(0:N-1)          :: vd
     real(wp)                            :: F_ext
     integer                             :: i, j, res
-    integer, parameter                  :: measurement_period = 100
+    integer, parameter                  :: measurement_period = 1000
     real(wp), parameter                 :: pi = 4.0_wp* atan(1.0_wp)
+
+    ! -- files
+    character(len=1024)             :: file_trajectory
     
     ! -- standard values
-    real(wp), parameter     :: std_r        = 12E-9_wp  ! m
+    real(wp), parameter     :: std_r        = 12e-9  !12E-9_wp  ! m
     real(wp), parameter     :: std_L        = 20E-6_wp  ! m
     real(wp), parameter     :: std_dU       = 80.0_wp   ! eV
     real(wp), parameter     :: std_alfa     = 0.2_wp
-    real(wp), parameter     :: std_tau      = 4.0_wp     
+    real(wp), parameter     :: std_tau      = 0.5_wp     
     real(wp), parameter     :: std_zeta     = 1E-3_wp   ! Pa*s
     real(wp), parameter     :: std_kT       = 0.026_wp  ! Termisk energi i eV
     real(wp), parameter     :: std_x0       = 0.0_wp
@@ -57,98 +61,95 @@ program biased_brownian_motion
     !real(wp), parameter     :: std_x0        = 0.0_wp
     !real, parameter         :: fraction_off  = 0.5_wp
     
-    
     !// Initialise the random seed
     call init_random_seed    
     
     !// Read in values from the user
     call input_potential()    
-    call input_particle(-1)        !// place this inside a loop to get individual radiuses and starting positions for the different particles; set -1 to i.
+    call input_particle(-1)        ! place inside loop to get individual r/x0
     gama = 6.0_wp*pi*zeta*r
     omega = dU/(gama*L**2)
 
     !// Criterion for the time step
     call get_dt()
-    write(*,*) "Timestep [s]: ", dt/omega
+    write(*,*) 'Timestep [s]: ', dt/omega
     write(*, fmt='(A,F0.1)') 'Total time [s]: ', dt*nSteps/omega
     
-    !// The external force due to gravity (?!)
-    F_ext = 0.0_wp     !- 4.0_wp/3.0_wp * pi * r**3 * (1.05e3 - 0.9982071e3_wp)*9.81*sin(pi/4.0_wp)
-    
-    do while (tau <= 0.5_wp)
+    !// Write to file
+    call write_constants_to_file()
+    call create_empty_trajectory_file()
+
+    do while (tau <= 5.0)
         write(*,*) tau
         !// Start the diffusion process for the N particles
         do i = 0, (N-1)
+            !if (mod(i, 10) == 0) then
+                !write(*,fmt='(I0, A)', advance='no') i, ' '
+            !end if
             
-            !// Set the starting position for the particles
-            x(i, 0) = x0
-
+            !// Set the starting position for the particles and then update x
+            x(0) = x0
             do j = 1, nSteps - 1
-                x(i, j) = updateX(x(i, j-1), j*dt, dt)
+                x(j) = updateX(x(j-1), j*dt, dt)
             end do
             
+            !call append_trajectory_to_file(file_trajectory)
+            vd(i) = (x(nSteps-1) - x(0))*L / (dt*nSteps/omega)
         end do
-        
         call append_drift_velocity_to_file()
-        tau = tau + 0.1_wp
+        tau = tau + 0.01_wp
     end do
-    
-    call write_constants_to_file()
-    call write_trajectory_to_file()
-    
     !call system ('python make_plot.py')
     
 contains    
 
 !
 ! Update the position of the particle using the Euler scheme
-real(wp) function updateX(x, t, dt)
-    real(wp), intent(in)        :: x, t, dt
+function updateX(x, t, dt) result(new_x)
+    real(wp), intent(in)    :: x, t, dt
+    real(wp)                :: new_x       
     
-    updateX = x + F(x, t) * dt + sqrt(2*kT/dU*dt)*random_gauss()
-    return
+    new_x = x + F(x, t) * dt + sqrt(2*kT/dU*dt)*random_gauss()
     
 end function
 
 
 !
 !Compute the potential U(x, t)
-real(wp) function U(xPos, time)
-    real(wp), intent(in)        :: time
-    real(wp), intent(in)        :: xPos
-    real(wp)                    :: x
-    real(wp)                    :: t
+function U(xPos, time) result(pot)
+    real(wp)                    :: pot
+    real(wp), intent(in)        :: time, xpos
+    real(wp)                    :: t, x
     
     x = xPos - floor(xPos)
     t = time - floor(time/(tau*omega))*tau*omega
     
     if (tau /= 0.0_wp .and. t >= 0.0_wp .and. t < fraction_off*tau*omega) then
-        U = 0.0_wp
+        pot = 0.0_wp
     else
         if (x >= 0.0_wp .and. x < alfa) then
-            U = x/alfa
+            pot = x/alfa
         else if (x >= alfa .and. x < 1.0_wp) then
-            U = (1.0_wp-x)/(1.0_wp-alfa)
+            pot = (1.0_wp-x)/(1.0_wp-alfa)
         end if
     end if
-    
-    return
+
 end function
 
 
 !
 ! Compute the force due to the potential U(x, t)
-real(wp) function F(x, t)
+function F(x, t) result(force)
+    real(wp)                    :: force
     real(wp), intent(in)        :: x, t
     real(wp)                    :: dx = 1.0e-3_wp
     
     if (U(x,t) == 0.0_wp) then
-        F = F_ext
+        force = 0.0_wp
     else
-        F = - ( U(x + dx, t) - U(x - dx, t) ) / (2.0_wp*dx) + F_ext
+        force = - ( U(x + dx, t) - U(x - dx, t) ) / (2.0_wp*dx)
     end if
     
-    return
 end function
 
 !
@@ -157,11 +158,11 @@ subroutine get_dt()
     dt = 0.1_wp
     
     if ( 1/alfa >= 1/(1-alfa) ) then
-        do while ( 1/alfa*dt + 4*sqrt(2*kT/dU*dt) > 0.1_wp*alfa )
+        do while ( 1/alfa*dt + 4*sqrt(2*kT/dU*dt) > 0.5_wp*alfa )
             dt = dt/2
         end do
     else 
-        do while ( 1/(1-alfa)*dt + 4*sqrt(2*kT/dU*dt) > 0.1_wp*alfa )
+        do while ( 1/(1-alfa)*dt + 4*sqrt(2*kT/dU*dt) > 0.5_wp*alfa )
             dt = dt/2
         end do
     end if
@@ -173,6 +174,92 @@ subroutine get_dt()
     end if
     
 end subroutine
+
+
+
+!
+! Append the drift velocitys to file (diffusion length per s) )
+subroutine append_drift_velocity_to_file()
+    real(wp)                :: avg_vd
+    real                    :: std_dev
+    logical                 :: exist
+    integer                 :: i
+    character(len=1024)     :: filename
+    write(filename, fmt='(A,I0,A, F0.0, A)') 'drift_velocity_N', N, '_t', dt*nSteps/omega, '.txt'
+    filename = trim(filename)
+
+    if (N >= 50) then
+        inquire(file=filename, exist=exist)
+        if (exist) then
+            open(12, file=filename, status="old", position="append", action="write")
+        else
+            open(12, file=filename, status="new", action="write")
+        end if
+    
+        avg_vd = sum(vd)/N
+
+        std_dev = 0.0_wp
+        do i = 0, N-1
+            std_dev = std_dev + (avg_vd - vd(i))**2
+        end do
+        std_dev = sqrt(std_dev/(N-1))
+
+        write(12, *) tau, avg_vd, std_dev
+        close(12)
+    else
+        write(*,*) 'The number of particles is too small to get an accurate measurment of the average drift velocity.'
+        write(*,*) 'You should use N >= 50. You have N = ', N
+    end if
+end subroutine
+
+subroutine create_empty_trajectory_file()
+    write(file_trajectory, fmt='(A,I0,A,F0.1,A, F0.2, A)') 'trajectory_N', N, '_rnm', r/1.0e-9, '_tau', tau, '.txt'
+    file_trajectory = trim(file_trajectory)
+    open(unit=13,file=file_trajectory, form="formatted", status="replace", action="write")
+    close(unit=13) 
+end subroutine
+    
+!
+! Writes the trajectory of the particle(s) to file
+subroutine append_trajectory_to_file(filename)
+    character(len=*), intent(in)   :: filename
+
+    open(unit=1, file=filename, status="old", position="append", action="write")
+    
+    write(1,*) x(0:nSteps-1:measurement_period)
+    
+    close(unit=1)
+    
+end subroutine
+
+
+!
+! Writes constants to file
+subroutine write_constants_to_file()
+    character(len=1024)            :: filename
+
+    write(filename, fmt='(A,I0,A,F0.1,A, F0.2, A)') 'constants_N', N, '_rnm', r/1.0e-9, '_tau', tau, '.txt'
+    filename = trim(filename)
+    
+    open(unit=13,file=filename, form="formatted", status="replace", action="write", iostat = res)
+    if (res /= 0) then
+        write(*,*) "Error in opening file, status", res
+        stop
+    end if
+
+    write(13,*) N, nSteps/measurement_period   !// #of particles, #of time steps
+    write(13,*) dt * measurement_period              !// delta_t
+    write(13,*) alfa                                !// alfa
+    write(13,*) tau                                 !// tau
+    write(13,*) zeta                                !// zeta
+    write(13,*) r                                   !// radius of particle
+    write(13,*) dU                                  !// dU
+    write(13,*) L                                   !// L
+    write(13,*) kT                                  !// kT
+    
+    close(unit=13)
+end subroutine
+
 
 
 !
@@ -234,7 +321,6 @@ subroutine input_w_default(param, default_param)
     real(wp), intent(in)  :: default_param
 
     integer, parameter              :: MAX_LENGTH   = 100
-    !character(len=*), parameter     :: format_0     = "(A)"
     character(len=MAX_LENGTH)       :: input_str
     real(wp)                        :: input_real
     integer                         :: flag
@@ -250,110 +336,16 @@ subroutine input_w_default(param, default_param)
 end subroutine
 
 
-!
-! Append the drift velocitys to file (diffusion length per s) )
-subroutine append_drift_velocity_to_file()
-    real(wp)                :: avg_vd
-    real                    :: std_dev
-    !real, dimension(0:N-1)  :: vd
-    logical                 :: exist
-    integer                 :: i
-    character(len=1024)     :: filename
-
-    write(filename, fmt='(A,F0.0,A,I0,A)') 'drift_velocity_t', dt*nSteps/omega, '_N', N, '.txt'
-    filename = trim(filename)
-
-    if (N >= 25) then 
-
-        inquire(file=filename, exist=exist)
-        if (exist) then
-            open(12, file=filename, status="old", position="append", action="write")
-        else
-            open(12, file=filename, status="new", action="write")
-        end if
-    
-        avg_vd = 0.0_wp
-    
-        do i = 0, N - 1
-            avg_vd = avg_vd + (x(i, nSteps-1) - x(i, 0))*L / (dt*nSteps/omega)
-        end do
-    
-        avg_vd = avg_vd / N
-        
-        std_dev = 0.0_wp
-        do i = 0, N-1
-            std_dev = std_dev + (avg_vd - (x(i, nSteps-1) - x(i, 0))*L / (dt*nSteps/omega))**2
-        end do
-            std_dev = sqrt(std_dev/(N-1))
-
-        write(12, *) tau, avg_vd, std_dev
-        close(12)
-    else
-        write(*,*) 'The number of particles is too small to get an accurate measurment of the average drift velocity.'
-        write(*,*) 'You should use N >= 25. You have N = ', N
-    end if
-end subroutine
-    
-    
-!
-! Writes the trajectory of the particle(s) to file
-subroutine write_trajectory_to_file()
-    character(len=1024)            :: filename
-
-    write(filename, fmt='(A,F0.3,A,I0,A)') 'trajectory_tau', tau, '_N', N, '.txt'
-    filename = trim(filename)
-
-    open(unit=1,file=filename, form="formatted", status="replace", action="write", iostat = res)
-    if (res /= 0) then
-        write(*,*) "Error in opening file, status", res
-        stop
-    end if
-    
-    do i = 0, size(x, dim=1) - 1
-        write(1,*) x(i,0:nSteps-1:measurement_period)        !// x = [[]]
-    end do
-    close(unit=1)
-    
-end subroutine
-
-
-!
-! Writes constants to file
-subroutine write_constants_to_file()
-    character(len=1024)            :: filename
-
-    write(filename, fmt='(A,F0.3,A,I0,A)') 'constants_tau', tau, '_N', N, '.txt'
-    filename = trim(filename)
-    
-    open(unit=13,file=filename, form="formatted", status="replace", action="write", iostat = res)
-    if (res /= 0) then
-        write(*,*) "Error in opening file, status", res
-        stop
-    end if
-
-    write(13,*) N, nSteps/measurement_period   !// #of particles, #of time steps
-    write(13,*) dt * measurement_period              !// delta_t
-    write(13,*) alfa                                !// alfa
-    write(13,*) tau                                 !// tau
-    write(13,*) zeta                                !// zeta
-    write(13,*) r                                   !// radius of particle
-    write(13,*) dU                                  !// dU
-    write(13,*) L                                   !// L
-    write(13,*) kT                                  !// kT
-    
-    close(unit=13)
-end subroutine
-
-
 
 !
 ! Gaussian random number
 ! Draw Gaussian distributed random numbers. Normal distribution is 
 ! implemented by the polar Box-Müller algorithm using the uniform 
 ! distribution from random_number().
-real(wp) function random_gauss()
-    real(wp)        :: rand1, rand2, w
-    integer            :: i
+function random_gauss() result(rand_gauss)
+    real(wp)            :: rand_gauss
+    real(wp)            :: rand1, rand2, w
+    integer             :: i
     
     w = 0.0_wp
     do while(w == 0.0_wp .OR. w >= 1)
@@ -364,7 +356,7 @@ real(wp) function random_gauss()
         w = rand1**2 + rand2**2
     end do
 
-    random_gauss = rand1 * SQRT( - 2.0_wp * LOG(w) / w)
+    rand_gauss = rand1 * SQRT( - 2.0_wp * LOG(w) / w)
 end function
 
 
--- assignment_01/make_plot.py
+++ assignment_01/make_plot.py
@@ -12,7 +12,10 @@ from matplotlib.colors import colorConverter
 import scipy
 from scipy.integrate import quad
 
+
+# ---------------------------------------------------------------------------
 # Read the constants from the file "constants.txt"
+# ---------------------------------------------------------------------------
 def read_constants(filename):
     fin = open(filename, 'r')
     N, nSteps = [int(x) for x in fin.readline().split()]     # read first line
@@ -35,9 +38,12 @@ def read_constants(filename):
     fin.close()
     return (N, nSteps, delta_t, alfa, tau, zeta, r, dU, L, kT)
 
+
+# ---------------------------------------------------------------------------
 # The potential
+# ---------------------------------------------------------------------------
 def potential(x, alfa):
-    x = x - np.floor(x/1.0)*1.0
+    x = x/scale_length - np.floor(x/scale_length/1.0)*1.0
     if (x >= 0.0 and x < alfa):
         U = x/alfa
     elif (x >= alfa and x < 1.0):
@@ -45,11 +51,13 @@ def potential(x, alfa):
     return U
 
 
+# ---------------------------------------------------------------------------
 # Plot the trajectory of the particle(s) and the potential they are in
-def plot_trajectory(data, alfa, tau):
+# ---------------------------------------------------------------------------
+def plot_trajectory(data, alfa, tau, realisation):
     dim1, dim2 = data.shape
-    minimum = data.min()
-    maximum = data.max()
+    minimum = -30e-6#data.min()
+    maximum = 720e-6#data.max()
     
     trajectory = plt.figure()
     define_color_cycle(trajectory)
@@ -57,10 +65,10 @@ def plot_trajectory(data, alfa, tau):
     # Plotting particles    
     y = np.linspace(0, (dim2 - 1), dim2)
     for i in range(0, dim1):
-        plt.plot(scale_length*1.0e6*data[i][:], scale_time*y, linewidth = 0.3)
+        plt.plot(1.0e6*data[i][:], scale_time*y, linewidth = 0.3)
     plt.ylabel(r'Time [s]', fontsize=15)
     plt.xlabel(r'Position [\mu m]', fontsize=15)
-    plt.axis([minimum*scale_length*1.0e6, maximum*scale_length*1.0e6, 0.0, y[-1]*scale_time])
+    plt.axis([minimum*1.0e6, maximum*1.0e6, 0.0, y[-1]*scale_time])
     plt.title(r'Trajectories of the particles with $\tau$ = %f'%(tau), fontsize = 20)
     
     # Plotting potential
@@ -71,7 +79,7 @@ def plot_trajectory(data, alfa, tau):
         pot[i] = scale_potential/kT*potential(x[i], alfa)            # Potential in factors of kT
 
     stuff = np.linspace(0, y[-1], nPoints)
-    X,Y = np.meshgrid(x*scale_length*1.0e6, stuff)
+    X,Y = np.meshgrid(x*1.0e6, stuff)
     Z = np.zeros((nPoints,nPoints))
     for i in range(0, nPoints):
         Z[i] = pot
@@ -81,46 +89,57 @@ def plot_trajectory(data, alfa, tau):
     cbar.set_label(r'Potential in factors of $k_B T$', fontsize = 15)
     
     # Saving figure
-    plt.savefig('fig/trajectory' + '.png');
+    plt.savefig('fig/trajectory' + realisation + '.png');
     
 
-# Plots the motion of the ensemble of particles and how the particle density behaves in time, 3D-plot
-def plot_ensemble_in_time(data):
+# ---------------------------------------------------------------------------
+# Plots the motion of the ensemble of particles and how the particle density
+# behaves in time, 3D-plot
+# ---------------------------------------------------------------------------
+def plot_ensemble_in_time(data, realisation):
     dim1, dim2 = data.shape
 
     ensemble_motion = plt.figure()
     ax = ensemble_motion.gca(projection='3d')
     
-    start = np.floor(data.min()) - (1.0-alfa)
-    stop = np.ceil(data.max()) + alfa
-    nbins = stop-start
+    start = np.floor(data.min()/scale_length)- (1.0-alfa)
+    stop = np.ceil(data.max()/scale_length) + alfa
+    print '[start, stop]', start, stop
+    nbins = np.ceil((stop-start))
+    start = scale_length*start
+    stop = scale_length*stop
+    print 'nbins', nbins
 
     n = 4
     
-    t = np.linspace(1000, dim2 - 1, n)
+    t = np.linspace(10, dim2 - 1, n)
     t = np.floor(t)
     
     verts = []
     verts2 = []
-    xs = np.arange(0, 3, 1)
     zs = np.linspace(0.0, 1.0*(n-1), n)
     hist_max = 0.0
     
     D = kT/gama
-    print 'D: ', D
+
+    vd = 0.0
+    for i in range(0, N):
+        vd = vd + (data[i, -1] - data[i, 0])/T/N        
 
     for z in zs:
         hist, x_bins = np.histogram(data[:, t[z]], bins=nbins, range=(start, stop), normed=False)
         if (max(hist) > hist_max):
             hist_max = max(hist)
-        x_center = scale_length*(x_bins[:-1] + x_bins[1:]) / 2
-        verts.append(zip(x_center*1e6, hist))
-        ax.bar(x_center, hist, align='center', zs=t[z]*scale_time, zdir='y', alpha=0.4, width = (x_center[1] - x_center[0]))
-        print 'time: ', t[z]*scale_time
-        print N/np.sqrt(4*np.pi*t[z]*scale_time*D)
-        hist_teo = N*np.exp(-np.power(x_center, 2)/(4*D*t[z]*scale_time))
-        print max(np.exp(-np.power(x_center, 2)/(4*D*t[z]*scale_time)))
+        x_center = (x_bins[:-1] + x_bins[1:]) / 2
+        print 'TIME: ', t[z]*scale_time
+        x_teo = x_center - vd*t[z]*scale_time
+        hist_teo = N/np.sqrt(4*np.pi*D*t[z]*scale_time)*np.exp(-np.power(x_teo, 2)/(4*D*t[z]*scale_time))
+        print 'MAX: ', max(hist_teo)
+        x_center = 1.0e6*x_center     # is now in micrometer
+        verts.append(zip(x_center, hist))
         verts2.append(zip( x_center,  hist_teo))
+        ax.bar(x_center, hist, align='center', zs=t[z]*scale_time, zdir='y', alpha=0.4, width = (x_center[1] - x_center[0]))
+        
     
     # Convert verts to poly
     cc = lambda c: colorConverter.to_rgba(c, alpha=0.6)
@@ -139,10 +158,14 @@ def plot_ensemble_in_time(data):
     ax.set_zlim3d(0.0, hist_max)
     
     # Saving figure
-    plt.savefig('fig/ensemble_motion' + '.png');
+    plt.savefig('fig/ensemble_motion' + realisation + '.png');
+    
+    return vd
 
 
+# ---------------------------------------------------------------------------
 # Defines a costumised color cycle for the plotting
+# ---------------------------------------------------------------------------
 def define_color_cycle(fig_name):
     # Define color cycle for the figure fig_name
     NUM_COLORS = 10
@@ -151,9 +174,10 @@ def define_color_cycle(fig_name):
     ax.set_color_cycle([cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
 
 
-
+# ---------------------------------------------------------------------------
 # Makes a histogram of where in the potential the particle have been and
 # compares it to the true Boltzmann distribution from statistical mechanics
+# ---------------------------------------------------------------------------
 def make_boltzmann_histogram(data,  dU, kT):
     # Plot histogram of Boltzmann distribution
     minimum = data.min()
@@ -174,8 +198,9 @@ def make_boltzmann_histogram(data,  dU, kT):
     plt.savefig('fig/boltzmann_distribution' + '.png');
     
 
-
+# ---------------------------------------------------------------------------
 # Checks if the gaussian Box-Muller algorithm really are a normal distribution
+# ---------------------------------------------------------------------------
 def plot_gaussian():
     normal_dist = lambda x: 1/np.sqrt(2*np.pi)*np.exp(-x**2/2)
     x = np.linspace(-5, 5, 1000)
@@ -192,19 +217,23 @@ def plot_gaussian():
     plt.savefig('fig/normal_distribution' + '.png');
     
 
-
-# Plots the drift velocity versus flashing period    
+# ---------------------------------------------------------------------------
+# Plots the drift velocity versus flashing period   
+# --------------------------------------------------------------------------- 
 def plot_drift_velocity(filename):
-    temp_arr = np.loadtxt(filename)     # drift velocity as total diffusion length per total diffusion time
+    temp_arr = np.loadtxt(filename)     # drift velocity in m/s
     temp_arr = temp_arr[temp_arr[:,0].argsort()]
     taus = temp_arr[:, 0]
-    drift_velocity = 1.0e6 * temp_arr[:, 1]
+    vd = 1.0e6 * temp_arr[:, 1]
     std_dev = 1.0e6 * temp_arr[:, 2]
-    tau_w_max_vel = taus[drift_velocity.argmax()]
+    tau_w_max_vel = taus[vd.argmax()]
     drift_vel = plt.figure()
-    plt.plot(taus, drift_velocity)
-    plt.errorbar(taus, drift_velocity, yerr=std_dev, fmt=None, color='b')
-    plt.ylabel(r'Drift velocity, $\left< v \right>$ [$\mathrm{\mu}$m/s]', fontsize=15)
+    plt.plot(taus, vd, 'b*')
+    plt.plot(taus, vd+std_dev, 'r*')
+    plt.plot(taus, vd-std_dev, 'r*')
+    plt.errorbar(taus, vd, yerr=std_dev, fmt=None, color='b')
+    plt.axis([0.0, max(taus), 0.0, 1.05*(max(vd)+max(std_dev))])
+    plt.ylabel(r'Average drift velocity, $\left< v_d \right>$ [$\mathrm{\mu}$m/s]', fontsize=15)
     plt.xlabel(r'Period of flash, $\tau$ [s]', fontsize=15)
     
     # Saving figure
@@ -213,13 +242,12 @@ def plot_drift_velocity(filename):
     return tau_w_max_vel
     
     
-    
 ############################################################################        
 ### ------------------------------- MAIN ------------------------------- ###
 ############################################################################
 
-do_plot_trajectory = False
-do_plot_ensemble_in_time = False
+do_plot_trajectory = True
+do_plot_ensemble_in_time = True
 do_plot_drift_velocity = True
 do_check_gaussian = False
 do_plot_boltzmann = False
@@ -229,26 +257,29 @@ do_plot_boltzmann = False
 std_tau = 0.1
 std_N   = 100
 
-file_const = 'constants_tau1.500_N100.txt'
-file_trajectory = 'trajectory_tau1.500_N100.txt'
-file_vd = 'drift_velocity_t11._N100.txt'
+realisation = '_N1000_rnm12.0_tau.50'
+file_const = 'constants' + realisation + '.txt'
+file_trajectory = 'trajectory' + realisation + '.txt'
+file_vd = 'drift_velocity' + realisation + '.txt'
 
 N, nSteps, delta_t, alfa, tau, zeta, r, dU, L, kT = read_constants(file_const)
 gama = 6.0*np.pi*zeta*r
 omega = dU/(gama*L**2)
 T = nSteps*delta_t/omega
-data = np.loadtxt(file_trajectory)
 
 # Convert from reduced units to real units with these scaling factors
 scale_length = L               # micrometer of real length
 scale_time = delta_t/omega     # time steps
 scale_potential = dU           # potential
 
+data = scale_length*np.loadtxt(file_trajectory)
+
 if do_plot_trajectory:
-    plot_trajectory(data, alfa, tau)
+    plot_trajectory(data, alfa, tau, realisation)
     
 if do_plot_ensemble_in_time:
-    plot_ensemble_in_time(data)
+    vd = plot_ensemble_in_time(data, realisation)
+    print 'The average drift velocity is: ', vd*1.0e6, 'micrometer/s'
 
 if do_plot_drift_velocity:
     tau_w_max_vel = plot_drift_velocity(file_vd)
