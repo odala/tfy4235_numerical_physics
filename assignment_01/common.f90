24679ea56eb35677d4a554d2e67244474db3a267
odala <odalauten@gmail.com>
2016-01-23 12:15:47 +0100

    Major git update due to fail by me...

---
+++ assignment_01/common.f90
@@ -0,0 +1,25 @@
+Module common
+
+    Implicit None
+    ! --- Define default precision.
+    Integer, Parameter      :: sp  = KIND(1.0)
+    Integer, Parameter      :: dp  = KIND(1.0D0)
+    Integer, Parameter      :: wp  = dp
+
+    ! --- Define global parameters
+    Real(wp), Parameter     :: pi = 4.0_wp*atan(1.0_wp)
+        
+    Type :: Parameter_Container
+    ! -----------------------------------------------------------
+    ! --- This is the container for the simulation parameters ---
+    ! -----------------------------------------------------------
+
+    Integer :: Nparticles, Nsteps
+    Real    :: radius, x0, tau, dU, L, alpha, zeta, kT, fraction_off, gama, omega
+
+    end Type Parameter_Container
+
+    ! --- The shared container named param.
+    Type(Parameter_Container), Save :: param
+
+end Module common
